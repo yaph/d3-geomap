710e47416d2b03a799c16ee1fa3c036dfe822ffc
Ramiro Gómez <code@ramiro.org>
2017-05-28 20:59:18 +0200

    Generate country topojson data

--- scripts/topo_countries.py
+++ scripts/topo_countries.py
@@ -6,9 +6,8 @@ from geonamescache import GeonamesCache
 gc = GeonamesCache()
 toposrc = '../data/states-provinces.json'
 
-#for iso2, country in gc.get_countries().items():
-for i in range(1):
-    iso3 = 'USA'  # country['iso3']
+for iso2, country in gc.get_countries().items():
+    iso3 = country['iso3']
 
     topojson = 'mapshaper -i {0} -filter \'"{1}" == adm0_a3\' -filter-fields fips,name -o format=topojson {1}.json'
     subprocess.call(topojson.format(toposrc, iso3), shell=True)
