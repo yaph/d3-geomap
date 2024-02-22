e1ec63ba17de0549b422893a36a4d70d6468070d
Ramiro Gómez <code@ramiro.org>
2024-02-22 23:06:37 +0100

    Upgrade dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
-import {terser} from 'rollup-plugin-terser';
 import postcss from 'rollup-plugin-postcss';
-import babel from 'rollup-plugin-babel';
+import babel from '@rollup/plugin-babel';
+import terser from '@rollup/plugin-terser';
 
 import * as meta from './package.json';
 
